c98db6a8a985667f754928afb9eb7a1d1b92d65a
Sitos <qiuqiucai001@gmail.com>
2026-02-18 21:48:45 +0800

    Test *8

--- static/index.js
+++ static/index.js
@@ -297,7 +297,7 @@ const MODE_NORMAL = 1, MODE_ENDLESS = 2, MODE_PRACTICE = 3;
     }
 
     function createTimeText(n) {
-        return 'TIME:' + n.toFixed(2);
+        return 'TIME:' + Math.ceil(n);
     }
 
     let _ttreg = / t{1,2}(\d+)/,
